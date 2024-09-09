2def4853bd92fca38f0de233be694d86fa280e7b
omer <xmen179@gmail.com>
2024-09-10 00:03:50 +0300

    son sonuncu

---
+++ src/main/java/com/project/payload/mappers/TourRequestMapper.java
@@ -0,0 +1,68 @@
+package com.project.payload.mappers;
+
+import com.project.entity.concretes.business.TourRequest;
+import com.project.payload.mappers.UserMapper;
+import com.project.payload.request.business.tourRequestRequests.TourRequestCreateAndUpdateRequest;
+import com.project.payload.response.business.TourRequestResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class TourRequestMapper {
+
+    private final UserMapper userMapper;
+    private final AdvertMapper advertMapper;
+
+    public TourRequestResponse tourRequestToTourRequestResponseForGuestUser(TourRequest request) {
+        return TourRequestResponse.builder()
+                .id(request.getId())
+                .tourDate(request.getTourDate())
+                .tourTime(request.getTourTime())
+                .status(request.getStatusStatus())
+                .updateAt(request.getUpdateAt())
+                //.advert(advertMapper.advertToAdvertResponseForTourRequest(tourRequest.getAdvert()))
+                //.ownerUser(userMapper.userToUserResponseForTourRequest(tourRequest.getOwnerUser()))
+
+                .build();
+    }
+
+    public TourRequestResponse tourRequestToTourRequestResponse(TourRequest request) {
+        return TourRequestResponse.builder()
+                .id(request.getId())
+                .tourDate(request.getTourDate())
+                .tourTime(request.getTourTime())
+                .status(request.getStatusStatus())
+                .updateAt(request.getUpdateAt())
+                //.advert(advertMapper.mapAdvertToAdvertResponseForTourRequest(tourRequest.getAdvert()))
+                .guestUser(userMapper.mapUserToUserResponseForTourRequest(request.getGuestUser()))
+                //.ownerUser(userMapper.toUserResponse(tourRequest.getOwnerUser()))
+                .build();
+    }
+
+
+
+
+
+    public TourRequest createTourRequestRequestToTourRequest(TourRequestCreateAndUpdateRequest request){
+        return TourRequest.builder()
+                .tourDate(request.getTourDate())
+                .tourTime(request.getTourTime())
+                .build();
+    }
+
+    public TourRequest TourRequestUpdateRequestToTourRequest(TourRequest tourRequest, TourRequestCreateAndUpdateRequest request) {
+    return tourRequest.toBuilder()
+            .tourDate(request.getTourDate())
+            .tourTime(request.getTourTime())
+            .updateAt(LocalDateTime.now())
+            .build();
+    }
+
+
+}
---
+++ src/main/java/com/project/payload/response/business/AdvertResponseForTourRequest.java
@@ -0,0 +1,21 @@
+package com.project.payload.response.business;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AdvertResponseForTourRequest {
+
+    private Long id;
+    private String title;
+    private Float price;
+    private String slug;
+
+}
